957a4aac002999d6edc549393d6bff3a0ebfda45
lyubov_saydenova <lyubov.bukhval@yandex.ru>
2023-05-01 13:24:02 +0300

    main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-x = int(input("x = "))
-y = int(input("y = "))
-if x > y:
-    print(f"Наибольшее число = {x}")
-if y > x:
-    print(f"Наибольшее число = {y}")
-if x == y:
-    print("Числа равны")

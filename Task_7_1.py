3c63b9fd179848038a147f7a5a9ae3a947947d29
lyubov_saydenova <lyubov.bukhval@yandex.ru>
2023-05-17 20:57:00 +0300

    Task_7_1

--- Task_7_1.py
+++ Task_7_1.py
@@ -1,15 +1,16 @@
-def gcd(a, b):
-    if b == 0:
-        return a
-    else:
-        return gcd(b, a % b)
-def lcm(a, b):
-    return a // gcd(a, b) * b
-n = int(input('Количество чисел: '))
-numbers = []
-for i in range(n):
-    numbers.append(int(input('n_{0}: '.format(i))))
-res = lcm(numbers[0], numbers[1])
-for i in range(2, n):
-    res = lcm(res, numbers[i])
-print(res)
+def noc(list):
+    def nod(a, b):
+        while a != b and a > 0:
+            a, b = max(a,b), min(a,b)
+            a = a % b
+        return b
+    def noc(a, b):
+        return a * b // nod(a, b)
+    y = 1
+    for i in list:
+        y = noc(y, i)
+    return y
+print(noc(list(map(int, input('-->').split()))))
+и ещё решение
+from math import gcd
+print(gcd(12, 18))

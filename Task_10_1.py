319ea6287f5b526dfaa41dc8533cfb4d39ece9b7
lyubov_saydenova <lyubov.bukhval@yandex.ru>
2023-05-25 22:16:22 +0300

    Task_10_1

---
+++ Task_10_1.py
@@ -0,0 +1,19 @@
+import openpyxl
+name_file = input('Введите название файла с расширением: ')
+wb = openpyxl.load_workbook(name_file)
+ws = wb.active
+d = {}
+for i in range(1, ws.max_row + 1):
+    name = ws.cell(i, 1).value
+    val = int(ws.cell(i, 2).value)
+    d[name] = d.get(name, 0) + val
+d['Итого'] = d.get('Итого', sum(d.values()))
+wb.create_sheet('Result')
+wb.active = wb['Result']
+ws = wb.active
+i = 1
+for k, v in d.items():
+    ws.cell(i, 1).value = k
+    ws.cell(i, 2).value = v
+    i += 1
+wb.save(name_file)

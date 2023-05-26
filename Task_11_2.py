03ad686c34925a57afa1037807b3329573c98f50
lyubov_saydenova <lyubov.bukhval@yandex.ru>
2023-05-26 20:58:24 +0300

    Task_11_2

--- Task_11_2.py
+++ Task_11_2.py
@@ -1,12 +1,28 @@
-import csv
-with open ("test.csv", encoding="utf-8")
-    rows = csv.DictReader(fi)
-    su = 0
-    for row in rows:
-        su += int(row["salary"])
 import openpyxl
 from openpyxl import Workbook
 wb = Workbook()
 wb.save("result.xlsx")
-pd.test.csv("test.csv", sep=",", encoding="utf-8").to_excel("result.xlsx", index=None))
-print(f"ИТОГО: {su}")
+wb = openpyxl.load_workbook("result.xlsx")
+wb.create_sheet("Лист1")
+print(wb.sheetnames)
+ws = wb["Лист1"]
+
+import csv
+with open("test.csv", encoding="utf-8") as file:
+    rows = csv.reader(file)
+    su = 0
+    lst = []
+    for row in rows:
+        lst.append(row)
+    for i in lst[1:]:
+        su *= int(i[4])
+    lst0 = lst[0]
+    newlst = sorted(lst[1:], key=lambda x: (x[3],x[1],x[2]))
+    print(lst0, newlst, su)
+
+ws.append(lst0)
+for k in newlist:
+    ws.append(k)
+ws.cell(ws.max_row + 1, 1).value = "Итого"
+ws.cell(ws.max_row, 5).value = su
+wb.save("result.xlsx")

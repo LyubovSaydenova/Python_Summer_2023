dde97cf8f2ab0ff8942076f224967c492473ee59
lyubov_saydenova <lyubov.bukhval@yandex.ru>
2023-06-28 21:58:33 +0300

    Task_22_1

--- Task_22_1.py
+++ Task_22_1.py
@@ -2,12 +2,19 @@ drop table if exists book
 create table book (
     title varchar,
     author varchar,
-    price integer
+    price integer,
+	amount integer
 );
 insert into book values
-('Война и мир', 'Л.Н.Толстой', 123),
-('Евгений Онегин', 'А.С.Пушкин', 234),
-('Анна Каренина', 'Л.Н.Толстой', 345),
-('Мёртвые души', 'Н.В.Гоголь', 456)
-select * from book order by author ASC
-select * from book order by price DESC
+('Война и мир', 'Толстой', 300, 5),
+('Евгений Онегин', 'Пушкин', 200, 10),
+('Анна Каренина', 'Толстой', 250, 4),
+('Мёртвые души', 'Гоголь', 200, 7)
+select title, author, amount,
+	(
+	select avg(amount)
+	from book
+	) as Среднее_количество
+from book
+where abs(amount - (select avg(amount) from book)) >25;
+select * from book order by author asc, price desc

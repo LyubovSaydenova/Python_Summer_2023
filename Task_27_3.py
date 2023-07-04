3b52909d55a39a72ebed8bf37e157cbd50938914
lyubov_saydenova <lyubov.bukhval@yandex.ru>
2023-07-04 22:11:50 +0300

    Task_27_3

---
+++ Task_27_3.py
@@ -0,0 +1,10 @@
+s = [1, 2, [11, 22, [111, 222, [1111, 2222, 3333], 333]]]
+r = []
+def rec_list(z):
+    for i in z:
+        if type(i) == list:
+            rec_list(i)
+        else:
+            r.append(i)
+rec_list(s)
+print(len(r))
